ffb0711dff64346bf969d7f54536a3a16c32893f
Sala 8 <sala8@FI-LabSistemas.ucatolica.loc>
2025-06-11 11:37:57 -0500

    Se trató de corregir la relación

--- data/models.py
+++ data/models.py
@@ -11,8 +11,8 @@ class Usuario(SQLModel, table=True):
     eliminado: bool = Field(default=False)
     vuelo_id: Optional[int] = Field(default=None, foreign_key="vuelo.id")
     mascota_id: Optional[int] = Field(default=None, foreign_key="mascota.id")
-    vuelo: Optional["Vuelo"] = Relationship(back_populates="usuarios", foreign_key=[vuelo_id])
-    mascota: Optional["Mascota"] = Relationship(back_populates="usuario", foreign_key=[mascota_id])
+    vuelo: Optional["Vuelo"] = Relationship(back_populates="usuarios")
+    mascota: Optional["Mascota"] = Relationship(back_populates="usuario")
 
 class Mascota(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -21,7 +21,7 @@ class Mascota(SQLModel, table=True):
     edad: int
     eliminado: bool = Field(default=False)
     usuario_id: Optional[int] = Field(default=None, foreign_key="usuario.id")
-    usuario: Optional[Usuario] = Relationship(back_populates="mascota", foreign_key=[usuario_id])
+    usuario: Optional[Usuario] = Relationship(back_populates="mascota")
 
 class Vuelo(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -31,4 +31,4 @@ class Vuelo(SQLModel, table=True):
     disponible: bool
     fecha: date
     usuario_id: Optional[int] = Field(default=None, foreign_key="usuario.id")
-    usuarios: List[Usuario] = Relationship(back_populates="vuelo", foreign_key=[usuario_id])
+    usuarios: List[Usuario] = Relationship(back_populates="vuelo")
