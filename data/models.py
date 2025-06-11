b6a5fd998b0a7605dba1f0989f5d25a994b1bd5f
Sala 8 <sala8@FI-LabSistemas.ucatolica.loc>
2025-06-11 12:11:26 -0500

    Se restablecieron los modelos pero ahora sin relaciones debido a conflictos

--- data/models.py
+++ data/models.py
@@ -1,24 +1,12 @@
-from sqlmodel import SQLModel, Field, Relationship
+from sqlmodel import SQLModel, Field
 from typing import Optional, List
 from datetime import date
-from sqlalchemy.orm import relationship
 
 class Usuario(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
     nombre: str
     cedula: str
     eliminado: bool = Field(default=False)
-    vuelo_id: Optional[int] = Field(default=None, foreign_key="vuelo.id")
-    mascota_id: Optional[int] = Field(default=None, foreign_key="mascota.id")
-
-    vuelo: Optional["Vuelo"] = Relationship(
-        back_populates="usuarios",
-        sa_relationship=relationship("Vuelo", back_populates="usuarios", foreign_keys=[vuelo_id])
-    )
-    mascota: Optional["Mascota"] = Relationship(
-        back_populates="usuario",
-        sa_relationship=relationship("Mascota", back_populates="usuario", foreign_keys=[mascota_id])
-    )
 
 class Mascota(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -26,11 +14,6 @@ class Mascota(SQLModel, table=True):
     raza: str
     edad: int
     eliminado: bool = Field(default=False)
-    usuario_id: Optional[int] = Field(default=None, foreign_key="usuario.id")
-    usuario: Optional[Usuario] = Relationship(
-        back_populates="mascotas",
-        sa_relationship=relationship("Usuario", back_populates="mascotas", foreign_keys=[usuario_id])
-    )
 
 class Vuelo(SQLModel, table=True):
     id: Optional[int] = Field(default=None, primary_key=True)
@@ -39,12 +22,3 @@ class Vuelo(SQLModel, table=True):
     precio: float
     disponible: bool
     fecha: date
-    usuarios: List[Usuario] = Relationship(
-        back_populates="vuelo",
-        sa_relationship=relationship("Usuario", back_populates="vuelo", foreign_keys=[Usuario.vuelo_id])
-    )
-
-Usuario.mascota = Relationship(back_populates="usuario")
-Usuario.vuelo = Relationship(back_populates="usuarios")
-Mascota.usuario = Relationship(back_populates="mascotas")
-Vuelo.usuarios = Relationship(back_populates="vuelo")
